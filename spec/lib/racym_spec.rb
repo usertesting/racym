a1ff28455f4a99932450e65455611abeae515a79
Justin Aiken <60tonangel@gmail.com>
2019-09-11 11:48:03 -0600

    Add basic specs

--- spec/lib/racym_spec.rb
+++ spec/lib/racym_spec.rb
@@ -1,5 +1,47 @@
 require_relative '../../lib/racym'
 
-describe "racym" do
+# Stub class for Rails.application.config
+class Rails
+  def self.application
+    self
+  end
+
+  def self.config
+    self
+  end
+end
+
+describe "#racym" do
+  context "with a single argument" do
+    before { allow(Rails).to receive(:foo).and_return :bar }
+
+    it "returns it" do
+      expect(racym :foo).to eq :bar
+    end
+  end
+
+  context "with a single argument that's a proc" do
+    before { allow(Rails).to receive(:foo).and_return ->{ :bar } }
+
+    it "calls it" do
+      expect(racym :foo).to eq :bar
+    end
+  end
+
+  context "with multiple args" do
+    before         { allow(Rails).to receive(:foo).and_return some_obj  }
+    let(:some_obj) { double "something", bar: :biz }
+
+    it "calls the chain" do
+      expect(racym :foo, :bar).to eq :biz
+    end
+  end
+end
+
+describe "#racym_set" do
+  pending
+end
+
+describe "#racym_undo!" do
   pending
 end
